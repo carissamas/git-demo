7ec6dbf51efd25b3c2e94852d78797dd2efd6f93
carissalondon <Carissa.Kurniawan75@thehutgroup.com>
2022-09-27 11:07:13 +0100

    Added Jupiter Feature

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ import sys
 def hello():
 	if sys.argv[1] == "Mars":
 		hellomars()
+	elif sys.argv[1] == "Jupiter":
+		hellowjupiter()
 	else:
 		helloworld()
 
@@ -12,4 +14,15 @@ def hellomars():
 
 def helloworld():
 	print("Hello, World")
+
+======
+
+	if sys.argv[1] == "Jupiter":
+		hellojupiter()
+	else:
+		helloworld()
+
+def hellojupiter():
+	print("Hello, Jupiter")
+
 
